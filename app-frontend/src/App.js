6ad6382ce477babe65600818dfd556ae34bc1655
Danis Khamidullin <danis.zp.123@gmail.com>
2021-08-17 11:13:48 +0300

    Removed useless stuff, added router

--- app-frontend/src/App.js
+++ app-frontend/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <div>about</div>
+          </Route>
+          <Route path="/users">
+            <div>users</div>
+          </Route>
+          <Route path="/">
+            <div>home</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
