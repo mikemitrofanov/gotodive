1b4f9c9ec6c0ac7171537e1b5e39f3a76c5c1e5d
Danis Khamidullin <danis.zp.123@gmail.com>
2021-08-27 13:00:35 +0300

    Added testing

--- app-frontend/src/index.js
+++ app-frontend/src/index.js
@@ -1,26 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router'
-import { NotificationContainer } from 'react-notifications';
-import history from './history';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import './assets/styles/main.scss';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import "bootstrap/dist/js/bootstrap.min.js"
-import 'react-notifications/lib/notifications.css';
+import App from './App'
 
-import Router from './Router';
-import { configureStore } from './store/configureStore'
-
-const store = configureStore()
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Router />
-      <NotificationContainer />
-    </ConnectedRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
