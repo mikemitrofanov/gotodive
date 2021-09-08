01f09d1bc4c863663ca80fffe31912ac028c7d51
shevchenkoandreydev <shevchenkoandrey.dev@gmail.com>
2021-09-08 16:51:10 +0300

    fix

--- frontend/components/PopularArticlesBlockList.js
+++ frontend/components/PopularArticlesBlockList.js
@@ -3,10 +3,10 @@ import Article from './Article'
 
 export default function PopularArticlesBlockList({ articles }) {
     return (
-        <div classNamess='flex justify-center'>
+        <div className='flex justify-center'>
             <div className='flex flex-col'>
                 <div className={`flex justify-center text-4xl fonts-title_popular-block`}>Популярное</div>
-                <div className='flex justify-center'>
+                <div className='flex '>
                     {articles.map((article) => <Article key={article.id} article={article} />)}
                 </div>
             </div>
