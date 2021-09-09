6b95a2ea92a49a067764ad4c543f24dd49947839
--global <--global>
2021-09-09 15:31:13 +0300

    fix

--- frontend/components/OurTeam.js
+++ frontend/components/OurTeam.js
@@ -2,12 +2,12 @@ import Image from "next/image"
 
 export default function OurTeam({teamName,desc,imagesValue}) {
     return (
-            <div className='flex-col m-32'>
+            <article className='flex-col m-32'>
                 <Image src={imagesValue} alt={`on this photo: ${teamName}`} height={390} width={389}/>
                 <div className='flex items-center flex-col mt-8'>
-                    <div className='name_team text-4xl	text-white'>{teamName}</div>
+                    <h2 className='name_team text-4xl	text-white'>{teamName}</h2>
                     <div className='desc-our_team text-xl text-white '>{desc}</div>
                 </div>
-            </div>
+            </article>
     )
 }
--- frontend/components/OurTeamList.js
+++ frontend/components/OurTeamList.js
@@ -2,13 +2,13 @@ import OurTeam from './OurTeam';
 
 export default function OurTeamList({dataTeam,images}) {
     return (
-        <div className='wrapper-our_team mt-40'>
-            <div className='flex flex-col'>
-                <div className='flex justify-center fonts-title_popular-block'>Наша Команда</div>
+        // <section className='wrapper-our_team mt-40'>
+            <section className='flex flex-col'>
+                <h1 className='flex justify-center fonts-title_popular-block'>Наша Команда</h1  >
                 <div className='h-wrapper  flex flex-row bg-custom justify-center mt-5'>
                     {dataTeam.map((data,i) => <OurTeam key={data.id} teamName={data.name} desc={data.desc} imagesValue={images[i].src}/>)}
                 </div>
-            </div>
-        </div>
+            </section>
+        // </section>
     )
 }
