78554c41a503dcd8d50b3c1b1ed4d190ac8951dd
shevchenkoandreydev <shevchenkoandrey.dev@gmail.com>
2021-09-09 09:51:58 +0300

    add contact form

--- frontend/components/ContactForm.js
+++ frontend/components/ContactForm.js
@@ -2,33 +2,12 @@ import InputForms from "./InputForms";
 import style from '../styles/contact-forms.module.css';
 
 export default function ContactForm() {
-    let formsParams = [
-        {
-            label: 'name',
-            type: 'text',
-        },
-        {
-            label: 'number',
-            type: 'text',
-        }, {
-            label: 'email',
-            type: 'email',
-        },
-    ]
-
-    let formsParamsMessage = [
-        {
-            label:'Сообщения',
-            type:'text'
-    },
-    ]
-
     return (
         <div className='flex justify-center'>
             <div className={`${style.wrapperContactForm}`}>
                 <div className={`flex justify-center fonts-title_popular-block`}>Пишите нам</div>
                     <form className='flex flex-col mt-7'>
-                                <InputForms params={formsParams} />
+                                <InputForms  />
                         <div className='flex justify-end mt-2'>
                             <button className={`${style.buttonSubmit}`}>Отправить</button>
                         </div>
--- frontend/components/InputForms.js
+++ frontend/components/InputForms.js
@@ -7,20 +7,20 @@ export default function InputForms(data) {
         <div className='flex flex-row'>
             <div className='flex flex-col pr-7'>
                 <div className='flex flex-col'>
-                    <label className={`${style.allParamsForms} mt-11`}>Имя</label>
+                    <label className={`${style.allParamsForms} mt-11 text-2xl`}>Имя</label>
                     <input type={'text'} name={'text'} className={`w-96 height-forms ${style.styleForms}`} />
                 </div>
                 <div className='flex flex-col'>
-                    <label className={`${style.allParamsForms} mt-11`}>Email</label>
+                    <label className={`${style.allParamsForms} mt-11 text-2xl`}>Email</label>
                     <input type={'email'} name={'email'} className={`w-96 height-forms ${style.styleForms}`} />
                 </div>
                 <div className='flex flex-col'>
-                    <label className={`${style.allParamsForms} mt-11`}>Phone</label>
+                    <label className={`${style.allParamsForms} mt-11 text-2xl`}>Phone</label>
                     <input type={'tel'} name={'phone'} className={`w-96 height-forms ${style.styleForms}`} />
                 </div>
             </div>
             <div>
-                <label className={`${style.allParamsForms} mt-11`}>Сообщение</label>
+                <label className={`${style.allParamsForms} mt-11 text-2xl`}>Сообщение</label>
                 <input type={'text'} name={'messages'} className={`${style.MessageInput} height-forms  ${style.styleForms}`} />
             </div>
         </div>
