2c62c79199b35f272c963988818771ceb8df424b
Serhii Kisil <areonks@gmail.com>
2021-09-21 14:31:33 +0300

    refactoring

--- backend/database/migrations/2021_09_08_075014_create_services_table.php
+++ backend/database/migrations/2021_09_08_075014_create_services_table.php
@@ -15,16 +15,16 @@ class CreateServicesTable extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->integer('order')->nullable();
             $table->string('title');
-            $table->text('description');
-            $table->string('short_description');
+            $table->integer('order')->nullable();
+            $table->string('description');
             $table->string('duration');
+            $table->integer('price');
+            $table->string('short_description');
             $table->string('min_age');
             $table->string('required_experience')->nullable();
             $table->string('max_depth')->nullable();
             $table->string('course_certificate')->nullable();
-            $table->integer('price');
             $table->boolean('is_popular')->default(false);
             $table->foreignId('service_category_id')->constrained('service_categories')->onDelete('cascade');
             $table->timestamps();
