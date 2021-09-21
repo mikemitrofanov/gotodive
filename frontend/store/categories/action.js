716c5c397d9677d3dd3117a4c1267d8cc77f4d7e
Serhii Kisil <areonks@gmail.com>
2021-09-21 13:08:23 +0300

    packages refactoring

--- frontend/store/categories/action.js
+++ frontend/store/categories/action.js
@@ -1,4 +1,4 @@
-import {createApi, fetchBaseQuery} from "@rtk-incubator/rtk-query";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
 
 export const categoriesApi = createApi({
     reducerPath: 'categoriesApi',
