ef7cf4314ffcd673114b19010c6364221d4e5d05
Serhii Kisil <areonks@gmail.com>
2021-09-27 15:11:32 +0300

    team block finished

--- frontend/components/Team.js
+++ frontend/components/Team.js
@@ -1,6 +1,27 @@
+import styles from '../styles/team.module.css'
+
 export default function Team() {
     return <div>
         <div className='component_title'>НАША КОМАНДА</div>
+        <div className={styles.main_container}>
+            <div className={styles.content_container}>
+                <div>
+                    <img src={'/images/ourteam/alexGerman.png'}/>
+                    <p className={styles.team_member_name}>Alex German</p>
+                    <p className={styles.team_member_description}>великий и ужасный</p>
+                </div>
+                <div>
+                    <img src={'/images/ourteam/yuriyPrihvatilo.png'}/>
+                    <p className={styles.team_member_name}>Юрии Прохватило</p>
+                    <p className={styles.team_member_description}>великий и ужасный</p>
+                </div>
+                <div>
+                    <img src={'/images/ourteam/rickSanches.png'}/>
+                    <p className={styles.team_member_name}>Рик Санчес</p>
+                    <p className={styles.team_member_description}>просто душка</p>
+                </div>
+            </div>
+        </div>
 
     </div>
 }
