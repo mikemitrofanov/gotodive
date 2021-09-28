36f93f5fa93ad19892a04a934f3c5a8e82f59b64
arturzhylindev <arturzhylindev@gmail.com>
2021-09-28 17:24:09 +0300

    Added mobile menu button

--- frontend/components/NavBar.js
+++ frontend/components/NavBar.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSearch, faUserPlus} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import CategoryServicesDropdown from "./CategoryServicesDropdown";
-import {data} from "../categoriesWithServices";
+import { data } from "../categoriesWithServices";
 import styles from '../styles/navBar.module.css'
 
 export default function NavBar() {
@@ -11,12 +11,13 @@ export default function NavBar() {
     return (
         <nav className={styles.top_nav_bar_block}>
             <div className={styles.main_block_container}>
+                <button className={styles.humburger_menu}></button>
                 <div className={''}>
                     <Link href='/'>
                         <a className={styles.main_nav_links}>Главная</a>
                     </Link>
                     {categories.map(category => (
-                        <CategoryServicesDropdown category={category} key={category.title}/>
+                        <CategoryServicesDropdown category={category} key={category.title} />
                     ))}
                     <Link href='/gallery'>
                         <a className={styles.main_nav_links}>Галлерея</a>
@@ -27,20 +28,20 @@ export default function NavBar() {
                 </div>
                 <div className={styles.top_search_block}>
                     <span className={styles.search_input_container}>
-                        <input className={styles.top_bar_search_input}/>
-                        <FontAwesomeIcon icon={faSearch}/>
+                        <input className={styles.top_bar_search_input} />
+                        <FontAwesomeIcon icon={faSearch} />
                     </span>
                     <Link href='/prices'>
-                        <a>RU</a>
+                        <a className={styles.languages} >RU</a>
                     </Link>
                     <Link href='/prices'>
-                        <a>UKR</a>
+                        <a className={styles.languages}  >UKR</a>
                     </Link>
                     <Link href='/prices'>
-                        <a>EN</a>
+                        <a className={styles.languages}  >EN</a>
                     </Link>
                     <Link href='/prices'>
-                        <a className={styles.top_bar_user_icon }><FontAwesomeIcon icon={faUserPlus}/></a>
+                        <a className={styles.top_bar_user_icon}><FontAwesomeIcon icon={faUserPlus} /></a>
                     </Link>
                 </div>
             </div>
