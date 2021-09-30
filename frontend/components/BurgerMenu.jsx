e98fc4be16d270cdf124cb50e8d1021a6f3cc84a
arturzhylindev <arturzhylindev@gmail.com>
2021-09-30 14:38:29 +0300

    Add burger menu

--- frontend/components/BurgerMenu.jsx
+++ frontend/components/BurgerMenu.jsx
@@ -1,55 +1,65 @@
-import { useState } from "react";
 import Link from "next/link";
-// import { slide as Menu } from "react-burger-menu";
-import { data } from "../categoriesWithServices";
 import styles from "../styles/navBar.module.css";
 
-export default function BurgerMenu() {
-  const [isOpen, setIsOpen] = useState(false);
-  const categories = data.data;
-  const showSettings = (event) => {
-    // event.preventDefault();
-    // console.log(event);
-  };
+export default function BurgerMenu({
+  isOpenedMainHeader,
+  openBurgerMenuAndCloseMainHead,
+  setIsOpened,
+  setIsOpenedMainHeader,
+  categories,
+}) {
 
   return (
-    <section className={styles["bm-menu-wrap"]} isOpen={isOpen}>
-      <nav className={styles.humburger_menu} onClick={showSettings}>
-        <Link href='/'>
-          <a className={styles.main_nav_links_burger}>Главная</a>
-        </Link>
-        {categories.map((category) => (
-          <span key={category.title} className={styles.dropdown_burger}>
+    <nav className={`${isOpenedMainHeader && styles.top_nav_bar_block_burger}`}>
+      <div
+        className={styles.wrapper_burger}
+        onClick={() =>
+          openBurgerMenuAndCloseMainHead({
+            event,
+            setIsOpened,
+            setIsOpenedMainHeader,
+          })
+        }
+      >
+        <div className={styles.backgraund_burger}>
+          <article className={styles.main_block_container_burger}>
             <Link href='/'>
-              <a className={styles.main_nav_links_burger}>{category.title}</a>
+              <a className={styles.main_nav_links_burger}>Главная</a>
             </Link>
-            <div className={styles.dropdown_content_burger}>
-              {category.services?.map((service) => (
-                <p key={service.title}>{service.title}</p>
-              ))}
+            {categories.map((category) => (
+              <span key={category.title} className={styles.dropdown_burger}>
+                <Link href='/'>
+                  <a className={styles.main_nav_links_burger}>
+                    {category.title}
+                  </a>
+                </Link>
+                <div className={styles.dropdown_content_burger}>
+                  {category.services?.map((service) => (
+                    <p key={service.title}>{service.title}</p>
+                  ))}
+                </div>
+              </span>
+            ))}
+            <Link href='/gallery'>
+              <a className={styles.main_nav_links_burger}>Галерея</a>
+            </Link>
+            <Link href='/prices'>
+              <a className={styles.main_nav_links_burger}>Прайс</a>
+            </Link>
+            <div className={styles.wrapper_languages_burger}>
+              <Link href='/prices'>
+                <a className={styles.languages_burger}>RU</a>
+              </Link>
+              <Link href='/prices'>
+                <a className={styles.languages_burger}>UKR</a>
+              </Link>
+              <Link href='/prices'>
+                <a className={styles.languages_burger}>EN</a>
+              </Link>
             </div>
-          </span>
-        ))}
-
-        <Link href='/gallery'>
-          <a className={styles.main_nav_links_burger}>Галлерея</a>
-        </Link>
-        <Link href='/prices'>
-          <a className={styles.main_nav_links_burger}>Прайс</a>
-        </Link>
-        <div className={styles.wrapper}>
-          <Link href='/prices'>
-            <a className={styles.languages_burger}>RU</a>
-          </Link>
-          <Link href='/prices'>
-            <a className={styles.languages_burger}>UKR</a>
-          </Link>
-          <Link href='/prices'>
-            <a className={styles.languages_burger}>EN</a>
-          </Link>
+          </article>
         </div>
-      </nav>
-
-    </section>
+      </div>
+    </nav>
   );
 }
--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,108 +1,83 @@
+import { useState } from "react";
 import Link from "next/link";
-// import BurgerMenu from "./BurgerMenu";
-import Test from "./Test";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import BurgerMenu from "./BurgerMenu";
 import CategoryServicesDropdown from "./CategoryServicesDropdown";
 import { data } from "../categoriesWithServices";
+import { openBurgerMenuAndCloseMainHead } from "../helper";
 import styles from "../styles/navBar.module.css";
 
 export default function NavBar() {
+  const [isOpened, setIsOpened] = useState(false);
+  const [isOpenedMainHeader, setIsOpenedMainHeader] = useState(true);
+
   const categories = data.data;
-  const openMenu = (event) => {
-    console.dir(event);
-  };
 
   return (
     <section>
-      <nav className={styles.top_nav_bar_block}>
-        <article className={styles.main_block_container}>
-          <button
-            className={styles.humburger_menu_btn}
-            onClick={openMenu}
-          ></button>
-          <div>
-            <Link href='/'>
-              <a className={styles.main_nav_links}>Главная</a>
-            </Link>
-            {categories.map((category) => (
-              <CategoryServicesDropdown
-                category={category}
-                key={category.title}
-              />
-            ))}
-            <Link href='/gallery'>
-              <a className={styles.main_nav_links}>Галлерея</a>
-            </Link>
-            <Link href='/prices'>
-              <a className={styles.main_nav_links}>Прайс</a>
-            </Link>
-          </div>
-          <div className={styles.top_search_block}>
-            <span className={styles.search_input_container}>
-              <input className={styles.top_bar_search_input} />
-              <FontAwesomeIcon
-                className={styles.color_search}
-                icon={faSearch}
-              />
-            </span>
-            <Link href='/prices'>
-              <a className={styles.languages}>RU</a>
-            </Link>
-            <Link href='/prices'>
-              <a className={styles.languages}>UKR</a>
-            </Link>
-            <Link href='/prices'>
-              <a className={styles.languages}>EN</a>
-            </Link>
-            <Link href='/prices'>
-              <a className={styles.top_bar_user_icon}></a>
-            </Link>
-          </div>
-        </article>
-      </nav>
-
-      <nav className={styles.top_nav_bar_block_burger}>
-        <article className={styles.main_block_container}>
-          <div>
-            <Link href='/'>
-              <a className={styles.main_nav_links_burger}>Главная</a>
-            </Link>
-            {categories.map((category) => (
-              <span key={category.title} className={styles.dropdown}>
-                <Link href='/'>
-                  <a className={styles.main_nav_links_burger}>
-                    {category.title}
-                  </a>
-                </Link>
-                <div className={styles.dropdown_content}>
-                  {category.services?.map((service) => (
-                    <p key={service.title}>{service.title}</p>
-                  ))}
-                </div>
+      <nav className={`${isOpened && styles.top_nav_bar_block}`}>
+        <div className={styles.wrapper_main_head}>
+          <article className={styles.main_block_container}>
+            <button
+              className={styles.humburger_menu_btn}
+              onClick={() =>
+                openBurgerMenuAndCloseMainHead({
+                  event,
+                  setIsOpened,
+                  setIsOpenedMainHeader,
+                })
+              }
+            ></button>
+            <div>
+              <Link href='/'>
+                <a className={styles.main_nav_links}>Главная</a>
+              </Link>
+              {categories.map((category) => (
+                <CategoryServicesDropdown
+                  category={category}
+                  key={category.title}
+                />
+              ))}
+              <Link href='/gallery'>
+                <a className={styles.main_nav_links}>Галерея</a>
+              </Link>
+              <Link href='/prices'>
+                <a className={styles.main_nav_links}>Прайс</a>
+              </Link>
+            </div>
+            <div className={styles.top_search_block}>
+              <span className={styles.search_input_container}>
+                <input className={styles.top_bar_search_input} />
+                <FontAwesomeIcon
+                  className={styles.color_search}
+                  icon={faSearch}
+                />
               </span>
-            ))}
-            <Link href='/gallery'>
-              <a className={styles.main_nav_links_burger}>Галлерея</a>
-            </Link>
-            <Link href='/prices'>
-              <a className={styles.main_nav_links_burger}>Прайс</a>
-            </Link>
-            <Link href='/prices'>
-              <a className={styles.languages}>RU</a>
-            </Link>
-            <Link href='/prices'>
-              <a className={styles.languages}>UKR</a>
-            </Link>
-            <Link href='/prices'>
-              <a className={styles.languages}>EN</a>
-            </Link>
-          </div>
-        </article>
+              <Link href='/prices'>
+                <a className={styles.languages}>RU</a>
+              </Link>
+              <Link href='/prices'>
+                <a className={styles.languages}>UKR</a>
+              </Link>
+              <Link href='/prices'>
+                <a className={styles.languages}>EN</a>
+              </Link>
+              <Link href='/prices'>
+                <a className={styles.top_bar_user_icon}></a>
+              </Link>
+            </div>
+          </article>
+        </div>
       </nav>
 
-      {/* <Test /> */}
-      {/* <BurgerMenu /> */}
+      <BurgerMenu
+        isOpenedMainHeader={isOpenedMainHeader}
+        openBurgerMenuAndCloseMainHead={openBurgerMenuAndCloseMainHead}
+        setIsOpened={setIsOpened}
+        setIsOpenedMainHeader={setIsOpenedMainHeader}
+        categories={categories}
+      />
     </section>
   );
 }
