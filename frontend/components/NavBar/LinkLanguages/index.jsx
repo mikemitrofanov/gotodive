8dcc66b34961d5d91b571d6834935355a5614d80
arturzhylindev <arturzhylindev@gmail.com>
2021-10-04 14:16:55 +0300

    Refactor links languages

---
+++ frontend/components/NavBar/LinkLanguages/index.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "./linkLanguages.module.css";
+
+export default function LinkLanguages({ isOpened, language }) {
+  return (
+    <Link href='/prices'>
+      <a className={`${isOpened ? styles.languages_burger : styles.languages}`}>
+        {language}
+      </a>
+    </Link>
+  );
+}
--- frontend/components/NavBar/LinksLanguages/index.jsx
+++ frontend/components/NavBar/LinksLanguages/index.jsx
@@ -1,24 +1,11 @@
-import Link from "next/link";
-import styles from './linksLanguages.module.css'
+import LinkLanguages from "../LinkLanguages";
 
 export default function LinksLanguages({ isOpened }) {
   return (
     <>
-      <Link href='/prices'>
-        <a className={`${ isOpened ? styles.languages_burger : styles.languages }`} >
-          RU
-        </a>
-      </Link>
-      <Link href='/prices'>
-        <a className={`${ isOpened ? styles.languages_burger : styles.languages }`} >
-          UKR
-        </a>
-      </Link>
-      <Link href='/prices'>
-        <a className={`${ isOpened ? styles.languages_burger : styles.languages }`} >
-          EN
-        </a>
-      </Link>
+      <LinkLanguages isOpened={isOpened} language='RU' />
+      <LinkLanguages isOpened={isOpened} language='UKR' />
+      <LinkLanguages isOpened={isOpened} language='EN' />
     </>
   );
 }
