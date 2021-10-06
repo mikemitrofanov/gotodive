a7db78a95e25297e9e75bc7a6e074ca5a0ba8e30
arturzhylindev <arturzhylindev@gmail.com>
2021-10-06 11:18:52 +0300

    Fixed opening menu

--- frontend/components/CategoryServicesDropdown/index.jsx
+++ frontend/components/CategoryServicesDropdown/index.jsx
@@ -1,25 +1,13 @@
-import { useState } from "react";
 import Link from "next/link";
 import styles from "./categoryServicesDropdown.module.css";
 
-export default function CategoryServicesDropdown({ category, isOpened }) {
-  const [isOpenedDropdown, setIsOpenedDropdown] = useState(false);
-
-  const checkOpenDropdown = (event) => {
-    // console.log(category);
-    // console.log(isOpened);
-    // console.log(event);
-    // console.log(isOpenedDropdown);
-
-    setIsOpenedDropdown((isOpenedDropdown) => !isOpenedDropdown);
-  };
-
+export default function CategoryServicesDropdown({ isOpened, checkOpenDropdown, category, style }) {
   return (
-    <span onClick={checkOpenDropdown} className={`${isOpened ? styles.dropdown_burger : styles.dropdown}`}>
+    <span onClick={(event) => checkOpenDropdown({ event })} className={`${isOpened ? styles.dropdown_burger : styles.dropdown}`}>
       <Link href='#'>
-        <a className={`${isOpened ? styles.main_nav_links_burger : styles.main_nav_links} ${isOpenedDropdown && styles.active_link_dropdown}`}>{category.title}</a>
+        <a className={`${isOpened ? styles.main_nav_links_burger : styles.main_nav_links} ${style && styles.active_link_dropdown}`}>{category.title}</a>
       </Link>
-      <div className={`${isOpened ? styles.dropdown_content_burger : styles.dropdown_content} ${isOpenedDropdown && styles.noHidden}`}>
+      <div className={`${isOpened ? styles.dropdown_content_burger : styles.dropdown_content} ${style && styles.noHidden}`}>
         {category.services?.map((service) => (
           <a key={service.title} href='#' className={`${isOpened ? styles.dropdown_links_burger : styles.dropdown_links}`}>
             {service.title}
--- frontend/components/LinksMenu/index.jsx
+++ frontend/components/LinksMenu/index.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
 import CategoryServicesDropdown from "../CategoryServicesDropdown";
 import LinkMenu from "../LinkMenu";
 
 export default function LinksMenu({ isOpened, categories }) {
+  const [isOpenedDropdownDiving, setIsOpenedDropdownDiving] = useState(false);
+  const [isOpenedDropdownSpecialization, setIsOpenedSpecialization] = useState(false);
+
+  const [diving, specialization] = categories;
+
+  const helperDiving = () => setIsOpenedDropdownDiving((setIsOpenedDropdownDiving) => !setIsOpenedDropdownDiving);
+  const helperSpecialization = () => setIsOpenedSpecialization((setIsOpenedSpecialization) => !setIsOpenedSpecialization);
+
+  const checkOpenDropdown = ({ event }) => {
+    if (event.target.innerText === "Дайвинг" && isOpenedDropdownSpecialization) {
+      helperSpecialization();
+    }
+
+    if (event.target.innerText === "Дайвинг") {
+      helperDiving();
+    }
+
+    if (event.target.innerText === "Специализация" && isOpenedDropdownDiving) {
+      helperDiving();
+    }
+
+    if (event.target.innerText === "Специализация") {
+      helperSpecialization();
+    }
+  };
+
   return (
     <>
       <LinkMenu isOpened={isOpened} href='/' name='Главная' />
-      {categories.map((category) => (
-        <CategoryServicesDropdown
-          category={category}
-          key={category.title}
-          isOpened={isOpened}
-        />
-      ))}
+      <CategoryServicesDropdown category={diving} style={isOpenedDropdownDiving} checkOpenDropdown={checkOpenDropdown} isOpened={isOpened} />
+      <CategoryServicesDropdown category={specialization} style={isOpenedDropdownSpecialization} checkOpenDropdown={checkOpenDropdown} isOpened={isOpened} />
       <LinkMenu isOpened={isOpened} href='/gallery' name='Галерея' />
       <LinkMenu isOpened={isOpened} href='/prices' name='Прайс' />
     </>
