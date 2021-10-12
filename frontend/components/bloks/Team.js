ab2dd9efc0abffe4899eb3b530b6474fd51e2c8e
arturzhylindev <arturzhylindev@gmail.com>
2021-10-12 16:53:03 +0300

    Added mobile version to component Team

--- frontend/components/bloks/Team.js
+++ frontend/components/bloks/Team.js
@@ -1,27 +1,28 @@
 import styles from '../../styles/team.module.css'
 
 export default function Team() {
-    return <div>
-        <div className='block_title'>НАША КОМАНДА</div>
-        <div className={styles.main_container}>
-            <div className={styles.content_container}>
-                <div>
-                    <img src={'/images/ourteam/alexGerman.png'}/>
-                    <p className={styles.team_member_name}>Alex German</p>
-                    <p className={styles.team_member_description}>великий и ужасный</p>
-                </div>
-                <div>
-                    <img src={'/images/ourteam/yuriyPrihvatilo.png'}/>
-                    <p className={styles.team_member_name}>Юрии Прохватило</p>
-                    <p className={styles.team_member_description}>великий и ужасный</p>
-                </div>
-                <div>
-                    <img src={'/images/ourteam/rickSanches.png'}/>
-                    <p className={styles.team_member_name}>Рик Санчес</p>
-                    <p className={styles.team_member_description}>просто душка</p>
+    return (
+        <section className={styles.background_block_title}>
+            <h1 className={styles.block_title}>НАША КОМАНДА</h1>
+            <div className={styles.main_container}>
+                <div className={styles.content_container}>
+                    <article className={styles.wrapper_user}>
+                        <img src={'/images/ourteam/alexGerman.png'} className={styles.img_team} />
+                        <p className={styles.team_member_name}>Alex German</p>
+                        <p className={styles.team_member_description}>великий и ужасный</p>
+                    </article>
+                    <article className={styles.wrapper_user}>
+                        <img src={'/images/ourteam/yuriyPrihvatilo.png'} className={styles.img_team} />
+                        <p className={styles.team_member_name}>Юрии Прохватило</p>
+                        <p className={styles.team_member_description}>великий и ужасный</p>
+                    </article>
+                    <article className={styles.wrapper_user}>
+                        <img src={'/images/ourteam/rickSanches.png'} className={styles.img_team} />
+                        <p className={styles.team_member_name}>Рик Санчес</p>
+                        <p className={styles.team_member_description}>просто душка</p>
+                    </article>
                 </div>
             </div>
-        </div>
-
-    </div>
+        </section>
+    )
 }
