01919997927798c53a2bdb90c87af6f23953a2ea
arturzhylindev <arturzhylindev@gmail.com>
2021-10-12 12:43:20 +0300

    Add navigation.min.css

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "../styles/swiper.min.css";
+import '../styles/navigation.min.css'
 import { Provider } from "react-redux";
 import { useStore } from "../store";
 
