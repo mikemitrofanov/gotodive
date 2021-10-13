2ec044250edee5661f64450d7fa20783b191e7f4
Serhii Kisil <areonks@gmail.com>
2021-10-13 16:06:39 +0300

    refactoring

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -20,7 +20,7 @@ class AuthServiceProvider extends ServiceProvider
      * Register any authentication / authorization services.
      *
      * @return void
-//     */
+     */
     public function boot()
     {
         $this->registerPolicies();
