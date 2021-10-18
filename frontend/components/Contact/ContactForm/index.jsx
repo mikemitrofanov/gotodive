2e3ed0ed1f898e0d9899c9d83a14a10a8b607c18
arturzhylindev <arturzhylindev@gmail.com>
2021-10-18 16:07:00 +0300

    Fixed distance between inputs

--- frontend/components/Contact/ContactForm/index.jsx
+++ frontend/components/Contact/ContactForm/index.jsx
@@ -6,12 +6,12 @@ import styles from "./contactForm.module.css";
 
 export default function ContactForm({ onSubmit }) {
   const validationSchema = Yup.object().shape({
-    email: Yup.string().email('Please enter a valid email. Must start with a lowercase letter, contain @ and "."').required("Email is required"),
+    email: Yup.string().email('Must contain @ and "."').required("Email is required"),
     phone: Yup.string()
-      .matches(/^(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d$/, "Please enter a valid phone number. Must contain at least 7 digits")
+      .matches(/^(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d$/, "Must contain at least 7 digits")
       .required("Phone is required"),
     name: Yup.string()
-      .matches(/^[A-Z][a-z]{1,64}$/, "Please enter a valid name. The first letter must be capitalized")
+      .matches(/^[A-Z][a-z]{1,64}$/, "The first letter must be capitalized")
       .required("Name is required"),
     message: Yup.string().required("Message required"),
   });
