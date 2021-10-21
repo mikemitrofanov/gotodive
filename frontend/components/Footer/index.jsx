e7a98ec7899ef3c490f4b8b67558135157ea9d6e
arturzhylindev <arturzhylindev@gmail.com>
2021-10-21 16:16:52 +0300

    Fif footer

--- frontend/components/Footer/index.jsx
+++ frontend/components/Footer/index.jsx
@@ -5,79 +5,82 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <section className={styles.wrapper_footer}>
-      <article>
-        <h1 className={styles.heading_contact_us}>Contact Us</h1>
-        <Link href='#'>
-          <a className={styles.contact_us}>
-            <img className={styles.icon} src='images/footer/icons/mapIcon.png' />
-            999, Моя Улица, Хорватия
-          </a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.contact_us}>
-            <img className={styles.icon} src='images/footer/icons/TelIcon.png' />
-            270-177-6026, 275-198-7978
-          </a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.contact_us}>
-            <img className={styles.icon} src='images/footer/icons/facebookIcon.png' />
-            @GotoDive
-          </a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.contact_us}>
-            <img className={styles.icon} src='images/footer/icons/mailIcon.png' />
-            GotoDive@gmail.com
-          </a>
-        </Link>
-      </article>
-      <article>
-        <h1 className={styles.heading_categories}>Categories</h1>
-        <Link href='#'>
-          <a className={styles.categories}>Дайвинг</a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.categories}>Обучение дайвингу</a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.categories}>Стань ДАЙВ ПРО</a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.categories}>Экскурсии</a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.categories}>Прайс</a>
-        </Link>
-      </article>
-      <article>
-        <h1 className={styles.heading_social}>Social</h1>
-        <Link href='#'>
-          <a className={styles.social}>
-            <img className={styles.icon} src='images/footer/icons/instaIcon.png' />
-            @GotoDive
-          </a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.social}>
-            <img className={styles.icon} src='images/footer/icons/telegramIcon.png' />
-            @GotoDive
-          </a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.social}>
-            <img className={styles.icon} src='images/footer/icons/wtsupIcon.png' />
-            @GotoDive
-          </a>
-        </Link>
-        <Link href='#'>
-          <a className={styles.social}>
-            <img className={styles.icon} src='images/footer/icons/viberIcon.png' />
-            @GotoDive
-          </a>
-        </Link>
-      </article>
-      <article>
+
+        <article className={styles.contact_us}>
+          <h1 className={styles.heading_contact_us}>Contact Us</h1>
+          <Link href='#'>
+            <a className={styles.contact_us}>
+              <img className={styles.icon} src='images/footer/icons/mapIcon.png' />
+              999, Моя Улица, Хорватия
+            </a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.contact_us}>
+              <img className={styles.icon} src='images/footer/icons/TelIcon.png' />
+              270-177-6026, 275-198-7978
+            </a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.contact_us}>
+              <img className={styles.icon} src='images/footer/icons/facebookIcon.png' />
+              @GotoDive
+            </a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.contact_us}>
+              <img className={styles.icon} src='images/footer/icons/mailIcon.png' />
+              GotoDive@gmail.com
+            </a>
+          </Link>
+        </article>
+        <article className={styles.contact_us}>
+          <h1 className={styles.heading_categories}>Categories</h1>
+          <Link href='#'>
+            <a className={styles.categories}>Дайвинг</a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.categories}>Обучение дайвингу</a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.categories}>Стань ДАЙВ ПРО</a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.categories}>Экскурсии</a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.categories}>Прайс</a>
+          </Link>
+        </article>
+        <article className={styles.contact_us}>
+          <h1 className={styles.heading_social}>Social</h1>
+          <Link href='#'>
+            <a className={styles.social}>
+              <img className={styles.icon} src='images/footer/icons/instaIcon.png' />
+              @GotoDive
+            </a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.social}>
+              <img className={styles.icon} src='images/footer/icons/telegramIcon.png' />
+              @GotoDive
+            </a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.social}>
+              <img className={styles.icon} src='images/footer/icons/wtsupIcon.png' />
+              @GotoDive
+            </a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.social}>
+              <img className={styles.icon} src='images/footer/icons/viberIcon.png' />
+              @GotoDive
+            </a>
+          </Link>
+        </article>
+
+
+      <article className={styles.gallery}>
         <h1 className={styles.heading_gallery}>Gallery</h1>
         <div className={styles.wrapper_gallery}>
           <a data-fancybox='gallery' data-src='images/footer/fancybox_photo/img1_big.png'>
