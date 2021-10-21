eaed1a5c4a11e235c9a40648271d09cb3b398101
arturzhylindev <arturzhylindev@gmail.com>
2021-10-21 17:21:44 +0300

    Starting work on locales

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,44 +1,34 @@
 import { categoriesApi, useGetPopularServicesQuery } from "../store/categories/action";
-import { initializeStore, removeUndefined } from "../store";
-import { useSelector } from "react-redux";
 import { withRedux } from "../hof/withRedux";
-import NavBar from "../components/NavBar";
-import SubHeader from "../components/SubHeader";
-import Popular from "../components/Popular";
-import Team from "../components/Team";
-import Contact from "../components/Contact";
+import { useState } from 'react'
 
-export default function Main({ isOpened, setIsOpened, language, setLanguage }) {
-
-  // console.log(JSON.stringify(data.categoriesApi.queries));
+export default function Main({ initialLanguage }) {
+  const [language, setLanguage] = useState(initialLanguage)
 
   const { data, error, isLoading } = useGetPopularServicesQuery(language);
 
-  console.log({
-    data,
-    error,
-    isLoading,
-  });
-
-
-
   return (
     <>
-      <NavBar isOpened={isOpened} setIsOpened={setIsOpened} setLanguage={setLanguage} />
-      <SubHeader isOpened={isOpened} />
-      <Popular language={language} />
-      <Team />
-      <Contact />
+      <select onChange={e => setLanguage(e.target.value)}>
+        <option value="ru">RU</option>
+        <option value="en">EN</option>
+        <option value="ua">UA</option>
+      </select>
+
+      {JSON.stringify(data)}
+
+      {/*<NavBar isOpened={isOpened} setIsOpened={setIsOpened} setLanguage={setLanguage} />*/}
+      {/*<SubHeader isOpened={isOpened} />*/}
+      {/*<Popular language={language} />*/}
+      {/*<Team />*/}
+      {/*<Contact />*/}
     </>
   );
 }
 
 export const getServerSideProps = withRedux(async (ctx, dispatch) => {
-  // const { data: categories } = useSelector(categoriesApi.endpoints.getAllCategories.select());
-  // const { data, error, isLoading } = useGetPopularServicesQuery("ru");
-  const store = initializeStore();
-  await store.dispatch(categoriesApi.endpoints.getAllCategories.initiate());
-  await store.dispatch(categoriesApi.endpoints.getPopularServices.initiate("ru"));
+  await dispatch(categoriesApi.endpoints.getAllCategories.initiate());
+  await dispatch(categoriesApi.endpoints.getPopularServices.initiate("ru"));
 
-  return { props: {} };
+  return { props: {initialLanguage: 'ru'} };
 });
