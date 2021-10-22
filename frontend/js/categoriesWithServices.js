4ae1a676a63f409ec687189538221ee726dd0226
arturzhylindev <arturzhylindev@gmail.com>
2021-10-22 15:05:00 +0300

    Fix data

--- frontend/js/categoriesWithServices.js
+++ frontend/js/categoriesWithServices.js
@@ -5,60 +5,13 @@ export const data = {
       "order": null,
       "services": [
         {
-          "title": "Non provident sunt et.",
-          "order": null,
-          "serviceCategory": 1,
-          "description": "Eveniet ratione dolore in repellendus. Assumenda ut quisquam sapiente qui vel. Minus ut culpa cum. Et expedita aspernatur sint in eum voluptatem. Nisi quo commodi molestiae nostrum quibusdam eius. Sed esse earum necessitatibus fugit facere. Qui omnis doloribus asperiores. Ab explicabo sit rerum porro in aperiam ullam.",
-          "duration": "28 минут",
-          "price": 799,
-          "isPopular": 0,
-          "short_description": "Recusandae eos rerum eos excepturi atque sapiente.",
-          "min_age": "30",
-          "required_experience": "",
-          "max_depth": "0",
-          "course_certificate": ""
+          "title": "Scuba diving",
         },
         {
-          "title": "Omnis at qui quibusdam.",
-          "order": null,
-          "serviceCategory": 1,
-          "description": "Omnis quia ut consequatur non aperiam eveniet. Distinctio cupiditate eos sed et vel voluptas natus dolor. Officia minus et dolorum nihil quia. Unde occaecati eos dolor commodi. Deserunt eveniet nihil placeat rerum aut ut sit. Dicta rerum quidem incidunt ad necessitatibus qui. Omnis ad fuga ea quia rerum saepe dolores tempore.",
-          "duration": "25 минут",
-          "price": 558,
-          "isPopular": 1,
-          "short_description": "Eum cupiditate voluptatem et ipsa. Voluptatem est maiores perspiciatis.",
-          "min_age": "15",
-          "required_experience": "",
-          "max_depth": "1",
-          "course_certificate": ""
+          "title": "Supervised Diver (Scuba Diver)",
         },
         {
-          "title": "Eos qui quasi vel.",
-          "order": null,
-          "serviceCategory": 1,
-          "description": "Nostrum culpa culpa quis doloremque voluptatibus alias sint. Est non explicabo sed omnis earum velit animi. Eum quo ut ipsa voluptas quia libero. Aut nostrum consectetur alias nisi atque et quaerat. Minus sunt et fugit placeat suscipit. Fugiat placeat pariatur esse.",
-          "duration": "27 минут",
-          "price": 631,
-          "isPopular": 0,
-          "short_description": "In a nesciunt qui dicta itaque molestiae. Totam delectus sint dolorem ab.",
-          "min_age": "25",
-          "required_experience": "",
-          "max_depth": "1",
-          "course_certificate": ""
-        },
-        {
-          "title": "Qui et dolore facilis.",
-          "order": null,
-          "serviceCategory": 1,
-          "description": "Facilis tenetur exercitationem sit expedita explicabo sunt et magni. Eos et omnis voluptas harum consequuntur eius eius. Sit facilis corrupti ut quia dolores sequi laborum provident. Porro soluta dolorum in non reiciendis.",
-          "duration": "16 минут",
-          "price": 345,
-          "isPopular": 0,
-          "short_description": "Quaerat aspernatur ut et qui. Voluptates necessitatibus doloremque culpa.",
-          "min_age": "29",
-          "required_experience": "",
-          "max_depth": "0",
-          "course_certificate": ""
+          "title": "Open Water Diver",
         }
       ]
     },
@@ -67,60 +20,28 @@ export const data = {
       "order": null,
       "services": [
         {
-          "title": "Qui ad dolore nihil.",
-          "order": null,
-          "serviceCategory": 2,
-          "description": "Fugiat est atque consequatur suscipit. Sapiente sed non sit laboriosam. Eos molestias veniam asperiores eos rem blanditiis. Maxime asperiores optio rem in.",
-          "duration": "4 минут",
-          "price": 850,
-          "isPopular": 1,
-          "short_description": "Doloribus consequatur labore minima quasi architecto aut et.",
-          "min_age": "11",
-          "required_experience": "",
-          "max_depth": "0",
-          "course_certificate": ""
+          "title": "Advanced Open Water Diver",
         },
         {
-          "title": "Sit occaecati ut quo.",
-          "order": null,
-          "serviceCategory": 2,
-          "description": "Occaecati enim illum commodi. Aut repellendus nulla nemo excepturi architecto enim omnis. Quia debitis laborum dolorem enim ut. Et tempore ducimus aut eos sequi vero. Temporibus accusantium nihil excepturi quam quaerat quasi nemo. Natus est ut non eius vero vel. Debitis in in explicabo laboriosam eos.",
-          "duration": "18 минут",
-          "price": 281,
-          "isPopular": 1,
-          "short_description": "Qui in repudiandae fugiat sapiente. Minus eaque est qui nobis praesentium ipsum.",
-          "min_age": "24",
-          "required_experience": "",
-          "max_depth": "1",
-          "course_certificate": "Dolor saepe et id ratione eum saepe enim. Aut quo totam non dicta velit nemo itaque molestiae. Corrupti dignissimos atque id quis rerum qui."
+          "title": "Recreational Trimix Diver",
         },
         {
-          "title": "Sequi omnis est aliquam.",
-          "order": null,
-          "serviceCategory": 2,
-          "description": "Similique ipsa cumque ut sapiente. Libero labore nesciunt recusandae aliquid dignissimos. Esse molestiae molestias corporis deserunt sit eligendi magni. Optio aut sint quae sint sed hic in veniam. Quis saepe molestias ut hic neque. Quasi earum atque magni natus optio voluptas. Ad ducimus nam nesciunt in quae delectus animi.",
-          "duration": "15 минут",
-          "price": 764,
-          "isPopular": 0,
-          "short_description": "Recusandae est est dolorem ea facilis nisi.",
-          "min_age": "7",
-          "required_experience": "",
-          "max_depth": "0",
-          "course_certificate": ""
+          "title": "Rescue Diver",
         },
         {
-          "title": "At ut veritatis et non.",
-          "order": null,
-          "serviceCategory": 2,
-          "description": "Eos corporis placeat earum ratione accusamus. Asperiores eum eius quia ea architecto et quos. Vero sunt blanditiis nemo ut. Odio consequatur nihil maxime et ullam quisquam. Et praesentium necessitatibus qui natus quibusdam et. Et quaerat quae aut et dolorem non quod.",
-          "duration": "13 минут",
-          "price": 743,
-          "isPopular": 0,
-          "short_description": "Deserunt rem est corporis impedit non. Maiores ad neque alias dolor.",
-          "min_age": "24",
-          "required_experience": "20 часов",
-          "max_depth": "1",
-          "course_certificate": "Amet omnis quisquam molestiae error. Est animi aut consequatur et temporibus. Fugit ut voluptas eum aliquam laboriosam. Iste quia nisi quia consequatur labore incidunt. Dolores delectus voluptates dolorem magni."
+          "title": "Elite Diver / Master Diver",
+        },
+        {
+          "title": "Complex Navigation Diver",
+        },
+        {
+          "title": "Deep Diver",
+        },
+        {
+          "title": "Drift Diver",
+        },
+        {
+          "title": "Dry suit diver",
         }
       ]
     },
