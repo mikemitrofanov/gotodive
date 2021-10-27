f9260195ea645bba29d2ce2137104e4b7f1a0043
arturzhylindev <arturzhylindev@gmail.com>
2021-10-27 17:25:22 +0300

    Fixed dropdown and arrows

--- frontend/components/NavBar/LinksMenu/CategoryServicesDropdown/index.jsx
+++ frontend/components/NavBar/LinksMenu/CategoryServicesDropdown/index.jsx
@@ -1,25 +1,80 @@
-import { faCaretRight, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useSelector } from "react-redux";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { useSelector, useDispatch } from "react-redux";
+import { changeStateSpecializationsDropdownMenu, stateSpecializationsDropdownMenu } from "../../../../store/slice/specializationsDropdownMenu";
+import { changeStateDivingDropdownMenu, stateDivingDropdownMenu } from "../../../../store/slice/divingDropdownMenu";
+import { useGetAllCategoriesQuery } from "../../../../store/categories/action";
+import { defaultLanguage } from "../../../../store/slice/defaultLanguageSlice";
+import { fakeDataNavMenu } from "../.././../../js/categoriesWithServices";
 import { stateBurgerMenu } from "../../../../store/slice/burgerMenuSlice";
+import { switchLanguage } from "../../../../helpers/switchLanguage";
 import styles from "./categoryServicesDropdown.module.css";
 
-export default function CategoryServicesDropdown({ checkOpenDropdown, category, style }) {
+export default function CategoryServicesDropdown() {
+  const isOpenSpecializationsDropdownMenu = useSelector(stateSpecializationsDropdownMenu);
+  const isOpenDivingDropdownMenu = useSelector(stateDivingDropdownMenu);
   const isOpenBurgerMenu = useSelector(stateBurgerMenu);
+  const language = useSelector(defaultLanguage);
+  const valueLanguages = switchLanguage(language);
+  const { data } = useGetAllCategoriesQuery(language);
+  const dropdown = data ? data : fakeDataNavMenu;
+  const dispatch = useDispatch();
+
+  const changeTheStateOfTheDivingDropdownMenu = ({ event }) => {
+    dispatch(changeStateDivingDropdownMenu());
+    if (isOpenSpecializationsDropdownMenu && event) {
+      dispatch(changeStateSpecializationsDropdownMenu());
+    }
+  };
+
+  const changeTheStateOfTheSpecializationsDropdownMenu = ({ event }) => {
+    dispatch(changeStateSpecializationsDropdownMenu());
+    if (isOpenDivingDropdownMenu && event) {
+      dispatch(changeStateDivingDropdownMenu());
+    }
+  };
 
   return (
-    <span onClick={(event) => checkOpenDropdown({ event })} className={`${isOpenBurgerMenu ? styles.dropdown_burger : styles.dropdown}`}>
-      {category.title === "Дайвинг" && isOpenBurgerMenu && <span>{style ? <FontAwesomeIcon className={styles.caret_down_burger_diving} icon={faCaretDown} /> : <FontAwesomeIcon className={styles.caret_right_burger_diving} icon={faCaretRight} />}</span>}
-      {category.title === "Специализации" && isOpenBurgerMenu && <span>{style ? <FontAwesomeIcon className={styles.caret_down_burger_specialization} icon={faCaretDown} /> : <FontAwesomeIcon className={styles.caret_right_burger_specialization} icon={faCaretRight} />}</span>}
-      <span className={`${isOpenBurgerMenu ? styles.main_nav_links_burger : styles.main_nav_links} ${style && styles.active_link_dropdown}`}>{category.title}</span>
-      <div className={`${isOpenBurgerMenu ? styles.dropdown_content_burger : styles.dropdown_content} ${style && styles.noHidden}`}>
-        {category.services?.map((service) => (
-          <a key={service.title} href='#' className={`${isOpenBurgerMenu ? styles.dropdown_links_burger : styles.dropdown_links}`}>
-            {service.title}
-          </a>
-        ))}
-      </div>
-      {!isOpenBurgerMenu && <span className={styles.wrapper_arrows}>{style ? <FontAwesomeIcon className={styles.caret_down} icon={faCaretDown} /> : <FontAwesomeIcon className={styles.caret_right} icon={faCaretRight} />}</span>}
-    </span>
+    <>
+      <span onClick={(event) => changeTheStateOfTheDivingDropdownMenu({ event })} className={`${isOpenBurgerMenu ? styles.dropdown_burger : styles.dropdown}`}>
+        <span className={`${isOpenBurgerMenu ? styles.main_nav_links_burger : styles.main_nav_links} ${isOpenDivingDropdownMenu && styles.active_link_dropdown}`}>
+          {dropdown.data ? dropdown.data[0].title : dropdown.dataDiving.titleDiving[valueLanguages]}
+          <FontAwesomeIcon className={`${isOpenDivingDropdownMenu ? styles.active_arrows : styles.arrows}`} icon={faPlay} />
+        </span>
+        <div className={`${isOpenBurgerMenu ? styles.dropdown_content_burger : styles.dropdown_content} ${isOpenDivingDropdownMenu && styles.noHidden}`}>
+          {dropdown.data
+            ? dropdown.data[0].services.map((service, index) => (
+                <a key={index} href='#' className={`${isOpenBurgerMenu ? styles.dropdown_links_burger : styles.dropdown_links}`}>
+                  {service.title}
+                </a>
+              ))
+            : dropdown.dataDiving.diving.map((service, index) => (
+                <a key={index} href='#' className={`${isOpenBurgerMenu ? styles.dropdown_links_burger : styles.dropdown_links}`}>
+                  {service}
+                </a>
+              ))}
+        </div>
+      </span>
+
+      <span onClick={(event) => changeTheStateOfTheSpecializationsDropdownMenu({ event })} className={`${isOpenBurgerMenu ? styles.dropdown_burger : styles.dropdown}`}>
+        <span className={`${isOpenBurgerMenu ? styles.main_nav_links_burger : styles.main_nav_links} ${isOpenSpecializationsDropdownMenu && styles.active_link_dropdown}`}>
+          {dropdown.data ? dropdown.data[1].title : dropdown.dataSpecializations.titleSpecializations[valueLanguages]}
+          <FontAwesomeIcon className={`${isOpenSpecializationsDropdownMenu ? styles.active_arrows : styles.arrows}`} icon={faPlay} />
+        </span>
+        <div className={`${isOpenBurgerMenu ? styles.dropdown_content_burger : styles.dropdown_content} ${isOpenSpecializationsDropdownMenu && styles.noHidden}`}>
+          {dropdown.data
+            ? dropdown.data[1].services.map((service, index) => (
+                <a key={index} href='#' className={`${isOpenBurgerMenu ? styles.dropdown_links_burger : styles.dropdown_links}`}>
+                  {service.title}
+                </a>
+              ))
+            : dropdown.dataSpecializations.specializations.map((service, index) => (
+                <a key={index} href='#' className={`${isOpenBurgerMenu ? styles.dropdown_links_burger : styles.dropdown_links}`}>
+                  {service}
+                </a>
+              ))}
+        </div>
+      </span>
+    </>
   );
 }
--- frontend/components/NavBar/LinksMenu/index.jsx
+++ frontend/components/NavBar/LinksMenu/index.jsx
@@ -1,49 +1,15 @@
-import { useSelector, useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
-import { changeStateSpecializationsDropdownMenu, stateSpecializationsDropdownMenu } from "../../../store/slice/specializationsDropdownMenu";
-import { changeStateDivingDropdownMenu, stateDivingDropdownMenu } from "../../../store/slice/divingDropdownMenu";
-import { useGetAllCategoriesQuery } from "../../../store/categories/action";
-import { defaultLanguage } from "../../../store/slice/defaultLanguageSlice";
 import CategoryServicesDropdown from "./CategoryServicesDropdown";
+
 import LinkMenu from "./LinkMenu";
 
-export default function LinksMenu({ categories }) {
-  const isOpenSpecializationsDropdownMenu = useSelector(stateSpecializationsDropdownMenu);
+export default function LinksMenu() {
   const { t } = useTranslation("translation", { useSuspense: false });
-  const isOpenDivingDropdownMenu = useSelector(stateDivingDropdownMenu);
-  const language = useSelector(defaultLanguage);
-  const { data } = useGetAllCategoriesQuery(language);
-  const [diving, specialization] = categories;
-  const dispatch = useDispatch();
-  console.log(data);
-
-  // const divingVariations = ["Дайвинг", "Дайвінг", "Diving"];
-  // const specializationsVariations = ["Специализации", "Спеціалізації", "Specializations"];
-
-  const changeStateDivingDropdownMenu = () => {
-    console.log("hcdsjskd");
-    dispatch(changeStateDivingDropdownMenu());
-  };
-
-  const changeStateSpecializationsDropdownMenu = () => {
-    dispatch(changeStateSpecializationsDropdownMenu());
-  };
-
-  const checkOpenDropdown = ({ event }) => {
-    console.log(event);
-    // if ((divingVariations.includes(event.target.innerText) && isOpenedDropdownSpecialization) || specializationsVariations.includes(event.target.innerText)) {
-    //   setIsOpenedSpecialization((setIsOpenedSpecialization) => !setIsOpenedSpecialization);
-    // }
-    // if ((specializationsVariations.includes(event.target.innerText) && isOpenedDropdownDiving) || divingVariations.includes(event.target.innerText)) {
-    //   setIsOpenedDropdownDiving((setIsOpenedDropdownDiving) => !setIsOpenedDropdownDiving);
-    // }
-  };
 
   return (
     <>
       <LinkMenu href='/' name={t("navBar.home")} />
-      <CategoryServicesDropdown onClick={() => changeStateDivingDropdownMenu()} category={diving} style={isOpenDivingDropdownMenu} language={language} />
-      <CategoryServicesDropdown onClick={changeStateSpecializationsDropdownMenu} category={specialization} style={isOpenSpecializationsDropdownMenu} language={language} />
+      <CategoryServicesDropdown />
       <LinkMenu href='/gallery' name={t("navBar.gallery")} />
       <LinkMenu href='/prices' name={t("navBar.price")} />
     </>
