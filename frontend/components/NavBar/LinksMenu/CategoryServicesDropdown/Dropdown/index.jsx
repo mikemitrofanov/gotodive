901483d7f9d7f8070951c149cf806ee8449d75e0
arturzhylindev <arturzhylindev@gmail.com>
2021-10-28 10:47:24 +0300

    Fix links dropdown

--- frontend/components/NavBar/LinksMenu/CategoryServicesDropdown/Dropdown/index.jsx
+++ frontend/components/NavBar/LinksMenu/CategoryServicesDropdown/Dropdown/index.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { useGetAllCategoriesQuery } from "../../../../../store/categories/action";
 import { defaultLanguage } from "../../../../../store/slice/defaultLanguageSlice";
 import { stateBurgerMenu } from "../../../../../store/slice/burgerMenuSlice";
+import { fakeDataNavMenu } from "../../../../../js/categoriesWithServices";
 import { switchLanguage } from "../../../../../helpers/switchLanguage";
 import styles from "./dropdown.module.css";
 
@@ -14,13 +15,17 @@ export default function Dropdown({ isOpenDropdownMenu, changeTheStateDropdownMen
   const { data } = useGetAllCategoriesQuery(language);
   const dropdown = data ? data : fakeDataNavMenu;
 
+  const doNotClose = ({ event }) => {
+    event.stopPropagation();
+  };
+
   return (
     <span onClick={(event) => changeTheStateDropdownMenu({ event })} className={`${isOpenBurgerMenu ? styles.dropdown_burger : styles.dropdown}`}>
       <span className={`${isOpenBurgerMenu ? styles.main_nav_links_burger : styles.main_nav_links} ${isOpenDropdownMenu && styles.active_link_dropdown}`}>
         {dropdown.data ? dropdown.data[index].title : dropdown.dataDiving.titleDiving[valueLanguages]}
         <FontAwesomeIcon className={`${isOpenDropdownMenu ? styles.active_arrows : styles.arrows}`} icon={faPlay} />
       </span>
-      <div className={`${isOpenBurgerMenu ? styles.dropdown_content_burger : styles.dropdown_content} ${isOpenDropdownMenu && styles.noHidden}`}>
+      <div onClick={(event) => doNotClose({ event })} className={`${isOpenBurgerMenu ? styles.dropdown_content_burger : styles.dropdown_content} ${isOpenDropdownMenu && styles.noHidden}`}>
         {dropdown.data
           ? dropdown.data[index].services.map((service, index) => (
               <a key={index} href='#' className={`${isOpenBurgerMenu ? styles.dropdown_links_burger : styles.dropdown_links}`}>
