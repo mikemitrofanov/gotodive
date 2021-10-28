b30cdae7ab432e18f9d54605161bac9c780497a0
Serhii Kisil <areonks@gmail.com>
2021-10-28 14:33:05 +0300

    swagger for photo in progress

--- backend/app/Http/Controllers/ServiceController.php
+++ backend/app/Http/Controllers/ServiceController.php
@@ -305,6 +305,57 @@ class ServiceController extends Controller
         return response()->noContent();
     }
 
+    /**
+     * @OA\Put(
+     *      path="/{language}/services/{service}/add-photo",
+     *      operationId="Add photo for service",
+     *      tags={"Photo"},
+     *      summary="Add Photo",
+     *      security={{"bearerAuth":{}}},
+     *      @OA\Parameter(
+     *          name="language",
+     *          description="Language code ",
+     *          required=true,
+     *          in="path",
+     *          example="en",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *     @OA\Parameter(
+     *          name="service",
+     *          description="Service Id",
+     *          required=true,
+     *          in="path",
+     *          example="1",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/CreateServiceRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/ShowServiceResponse")
+     *       ),
+     *       @OA\Response(
+     *          response=400,
+     *          description="Language code is not supported.",
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *        @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     */
+
     public function addPhotos($language, Service $service, AddPhotoRequest $request)
     {
         foreach ($request->photos as $photo) {
