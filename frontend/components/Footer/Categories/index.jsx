a24786f537244bcc660ae55f23609c3f9304f237
arturzhylindev <arturzhylindev@gmail.com>
2021-10-29 13:55:34 +0300

    Refactor component categories

--- frontend/components/Footer/Categories/index.jsx
+++ frontend/components/Footer/Categories/index.jsx
@@ -1,25 +1,46 @@
+import { useSelector } from "react-redux";
 import Link from "next/link";
+import { defaultLanguage } from "../../../store/slices/defaultLanguage";
+import { useGetAllCategoriesQuery } from "../../../store/categories/action";
+import { fakeDataFooter } from "../../../fakeDatabase/fakeDataFooter";
+import { switchLanguage } from "../../../helpers/switchLanguage";
 import styles from "./categories.module.css";
 
 export default function Categories() {
+  const { diving, divingTraining, becomeDivePro, excursions, price } = fakeDataFooter;
+  const language = useSelector(defaultLanguage);
+  const valueLanguages = switchLanguage(language);
+  const { data } = useGetAllCategoriesQuery(language);
+  const categories = data ? data : fakeDataFooter;
+
   return (
     <article className={styles.categories_wrapper}>
       <h1 className={styles.categories_heading}>Categories</h1>
-      <Link href='#'>
-        <a className={styles.categories}>Дайвинг</a>
-      </Link>
-      <Link href='#'>
-        <a className={styles.categories}>Обучение дайвингу</a>
-      </Link>
-      <Link href='#'>
-        <a className={styles.categories}>Стань ДАЙВ ПРО</a>
-      </Link>
-      <Link href='#'>
-        <a className={styles.categories}>Экскурсии</a>
-      </Link>
-      <Link href='#'>
-        <a className={styles.categories}>Прайс</a>
-      </Link>
+      {categories.data ? (
+        categories.data.map((category) => (
+          <Link href='#'>
+            <a className={styles.categories}>{category.title}</a>
+          </Link>
+        ))
+      ) : (
+        <>
+          <Link href='#'>
+            <a className={styles.categories}>{diving[valueLanguages]}</a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.categories}>{divingTraining[valueLanguages]}</a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.categories}>{becomeDivePro[valueLanguages]}</a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.categories}>{excursions[valueLanguages]}</a>
+          </Link>
+          <Link href='#'>
+            <a className={styles.categories}>{price[valueLanguages]}</a>
+          </Link>
+        </>
+      )}
     </article>
   );
 }
