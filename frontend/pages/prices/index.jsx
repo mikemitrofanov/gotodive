b5a2542d4a97828e5ba75558c6267eefe642fa47
arturzhylindev <arturzhylindev@gmail.com>
2021-11-01 12:45:14 +0200

    Fix paths

--- frontend/pages/prices/index.jsx
+++ frontend/pages/prices/index.jsx
@@ -1,5 +1,5 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { setDefaultLanguage } from "../../store/slice/defaultLanguageSlice";
+import { setDefaultLanguage } from "../../store/slices/defaultLanguage";
 import { categoriesApi } from "../../store/categories/action";
 import { withRedux } from "../../hof/withRedux";
 import NavBar from "../../components/NavBar";
