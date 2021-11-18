9b19ea95481d5f34e786dfd695c04d21dcfbd6f4
dmytroshylodev <dmytroshylodev@gmail.com>
2021-11-18 16:37:00 +0200

    add locales for the footer

--- frontend/components/Footer/Gallery/index.jsx
+++ frontend/components/Footer/Gallery/index.jsx
@@ -1,36 +1,39 @@
 import "@fancyapps/ui";
 import styles from "./gallery.module.css";
 
-export default function Gallery() {
-  return (
-    <article className={styles.gallery_wrapper}>
-      <h1 className={styles.gallery_heading}>Gallery</h1>
-      <div className={styles.wrapper_img}>
-        <a data-fancybox='' data-src='/images/footer/gallery/img1_big.png'>
-          <img className={styles.img_gallery} src='/images/footer/gallery/img1.png' />
-        </a>
-        <a data-fancybox='' data-src='/images/footer/gallery/img2_big.png'>
-          <img className={styles.img_gallery} src='/images/footer/gallery/img2.png' />
-        </a>
-        <a data-fancybox='' data-src='images/footer/gallery/img3_big.png'>
-          <img className={styles.img_gallery} src='/images/footer/gallery/img3.png' />
-        </a>
-        <a data-fancybox='' data-src='/images/footer/gallery/img4_big.png'>
-          <img className={styles.img_gallery} src='/images/footer/gallery/img4.png' />
-        </a>
-      </div>
-      <a data-fancybox='' data-src='images/footer/gallery/img5_big.png'>
-        <img className={styles.img_gallery} src='/images/footer/gallery/img5.png' />
-      </a>
-      <a data-fancybox='' data-src='/images/footer/gallery/img6_big.png'>
-        <img className={styles.img_gallery} src='/images/footer/gallery/img6.png' />
-      </a>
-      <a data-fancybox='' data-src='/images/footer/gallery/img7_big.png'>
-        <img className={styles.img_gallery} src='/images/footer/gallery/img7.png' />
-      </a>
-      <a data-fancybox='' data-src='/images/footer/gallery/img8_big.png'>
-        <img className={styles.img_gallery} src='/images/footer/gallery/img8.png' />
-      </a>
-    </article>
-  );
+export default function Gallery({title}) {
+
+    return (
+        <article className={styles.gallery_wrapper}>
+            <h1 className={styles.gallery_heading}>
+                {title}
+            </h1>
+            <div className={styles.wrapper_img}>
+                <a data-fancybox='' data-src='/images/footer/gallery/img1_big.png'>
+                    <img className={styles.img_gallery} src='/images/footer/gallery/img1.png'/>
+                </a>
+                <a data-fancybox='' data-src='/images/footer/gallery/img2_big.png'>
+                    <img className={styles.img_gallery} src='/images/footer/gallery/img2.png'/>
+                </a>
+                <a data-fancybox='' data-src='images/footer/gallery/img3_big.png'>
+                    <img className={styles.img_gallery} src='/images/footer/gallery/img3.png'/>
+                </a>
+                <a data-fancybox='' data-src='/images/footer/gallery/img4_big.png'>
+                    <img className={styles.img_gallery} src='/images/footer/gallery/img4.png'/>
+                </a>
+            </div>
+            <a data-fancybox='' data-src='images/footer/gallery/img5_big.png'>
+                <img className={styles.img_gallery} src='/images/footer/gallery/img5.png'/>
+            </a>
+            <a data-fancybox='' data-src='/images/footer/gallery/img6_big.png'>
+                <img className={styles.img_gallery} src='/images/footer/gallery/img6.png'/>
+            </a>
+            <a data-fancybox='' data-src='/images/footer/gallery/img7_big.png'>
+                <img className={styles.img_gallery} src='/images/footer/gallery/img7.png'/>
+            </a>
+            <a data-fancybox='' data-src='/images/footer/gallery/img8_big.png'>
+                <img className={styles.img_gallery} src='/images/footer/gallery/img8.png'/>
+            </a>
+        </article>
+    )
 }
--- frontend/components/Footer/index.jsx
+++ frontend/components/Footer/index.jsx
@@ -3,14 +3,17 @@ import Categories from "./Categories";
 import ContactUs from "./ContactUs";
 import Gallery from "./Gallery";
 import Social from "./Social";
+import {useTranslation} from "next-i18next";
 
 export default function Footer() {
-  return (
-    <section className={styles.footer_wrapper}>
-      <ContactUs />
-      <Categories />
-      <Social />
-      <Gallery />
-    </section>
-  );
+    const { t } = useTranslation("common");
+
+    return (
+        <section className={styles.footer_wrapper}>
+            <ContactUs title={t("footer.contactUs")}/>
+            <Categories title={t("footer.categories")}/>
+            <Social title={t("footer.social")}/>
+            <Gallery title={t("footer.gallery")}/>
+        </section>
+    )
 }
