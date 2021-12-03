114f180bc30fbb34425da1cb126990f5737e66e6
dmytroshylodev <dmytroshylodev@gmail.com>
2021-12-03 18:37:24 +0200

    add a button to clear the input when the text is entered into it

--- frontend/components/navbar/searchInput/index.jsx
+++ frontend/components/navbar/searchInput/index.jsx
@@ -1,11 +1,18 @@
 import styles from "./searchInput.module.css";
+import {useState} from "react";
 
 export default function SearchInput() {
+    const [searchQuery, setSearchQuery] = useState('');
 
     return (
-        <div className={styles.wrap_input}>
-            <input className={styles.search_input}/>
-            <a className={styles.icon_magnifier}/>
-        </div>
+        <form className={styles.wrap_input}>
+            <input className={styles.search_input}
+                   type="text"
+                   onChange={(e) => setSearchQuery(e.target.value)}
+                   value={searchQuery}
+            />
+            {searchQuery && <span className={styles.clear} onClick={() => setSearchQuery('')}/>}
+            <button className={styles.icon_magnifier} type="submit"/>
+        </form>
     )
 }
