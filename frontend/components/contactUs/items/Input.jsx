40f7cd9150d7ff906440ac3c73a4a9ab4e54713d
dmytroshylodev <dmytroshylodev@gmail.com>
2021-12-03 13:28:20 +0200

    add an attribute for the input autocomplete = "off"

--- frontend/components/contactUs/items/Input.jsx
+++ frontend/components/contactUs/items/Input.jsx
@@ -6,7 +6,7 @@ export default function Input({name, label}) {
     return (
         <div className={styles.wrap_input}>
             <label className={styles.label} htmlFor={name}>{label}</label>
-            <Field className={styles.input} name={name}/>
+            <Field className={styles.input} name={name} autocomplete="off"/>
             <ErrorMessage className={styles.error} name={name} component='div'/>
         </div>
     )
