c17e7bb6c72a961c4b3bb2503af5848046a7d579
dmytroshylodev <dmytroshylodev@gmail.com>
2021-12-12 23:54:45 +0200

    Search implementation, file structure changes for the Header component, code refactoring

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -2,9 +2,5 @@ module.exports = {
     i18n: {
         locales: ['ru', 'uk', 'en'],
         defaultLocale: 'ru'
-    },
-    react: {
-        useSuspense: false,
-        wait: true
     }
 }
--- frontend/redux/slices/apiSlice.js
+++ frontend/redux/slices/apiSlice.js
@@ -1,73 +1,83 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query'
-import { createApi } from '@reduxjs/toolkit/query/react'
+import {fetchBaseQuery} from '@reduxjs/toolkit/query'
+import {createApi} from '@reduxjs/toolkit/query/react'
 
 const url = process.env.NEXT_PUBLIC_URL
 const apiUrl = typeof window === 'object' ? process.env.NEXT_PUBLIC_API_URL : process.env.NEXT_INTERNAL_API_URL
 
 export const apiSlice = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({
-    baseUrl: apiUrl,
-  }),
-  endpoints: (build) => ({
-    getAllCategories: build.query({
-      query: (language) => `${language}/service-categories/services`,
-      transformResponse: response => {
-        return response.data
-      }
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: apiUrl,
     }),
+    endpoints: (build) => ({
+        getAllCategories: build.query({
+            query: (language) => `${language}/service-categories/services`,
+            transformResponse: response => {
+                return response.data
+            }
+        }),
 
-    getPopularServices: build.query({
-      query: (language) => `${language}/services/popular`,
-      transformResponse: response => {
-        return response.data
-      }
-    }),
+        getPopularServices: build.query({
+            query: (language) => `${language}/services/popular`,
+            transformResponse: response => {
+                return response.data
+            }
+        }),
 
-    getServices: build.query({
-      query: ({ language, id }) => `${language}/services/${id}`,
-      transformResponse: response => {
-        let photos = []
+        getServices: build.query({
+            query: ({language, id}) => `${language}/services/${id}`,
+            transformResponse: response => {
+                let photos = []
 
-        for (let i = 0; i < 4; i++) {
-          response.data.photos[i]?.optimized_photo_url
-            ? photos = [...photos, {
-              optimized_photo_url: `${url}/${response.data.photos[i].optimized_photo_url}`,
-              id: i + 1
-            }]
-            : photos = [...photos, {
-              optimized_photo_url: `${url}/${response.data.photos[0].optimized_photo_url}`,
-              id: i + 1
-            }]
-        }
+                for (let i = 0; i < 4; i++) {
+                    response.data.photos[i]?.optimized_photo_url
+                        ? photos = [...photos, {
+                            optimized_photo_url: `${url}/${response.data.photos[i].optimized_photo_url}`,
+                            id: i + 1
+                        }]
+                        : photos = [...photos, {
+                            optimized_photo_url: `${url}/${response.data.photos[0].optimized_photo_url}`,
+                            id: i + 1
+                        }]
+                }
 
-        let data = response.data;
+                let data = response.data;
 
-        ['title', 'description', 'duration', 'min_age', 'required_experience', 'max_depth',
-          'certification_requirements', 'min_logged_dives', 'max_end', 'course_certificate']
-          .forEach(param => {
+                ['title', 'description', 'duration', 'min_age', 'required_experience', 'max_depth',
+                    'certification_requirements', 'min_logged_dives', 'max_end', 'course_certificate']
+                    .forEach(param => {
 
-            if (!data[param]) data[param] = null
-          })
+                        if (!data[param]) data[param] = null
+                    })
 
-        return { ...data, photos }
-      }
-    }),
+                return {...data, photos}
+            }
+        }),
 
-    getPhotoGallery: build.query({
-      query: () => '/photos',
-      transformResponse: response => response.data.map(photo => ({
-        ...photo,
-        photo_url: `${url}/${photo.photo_url}`,
-        optimized_photo_url: `${url}/${photo.optimized_photo_url}`
-      }))
+        getPhotoGallery: build.query({
+            query: () => '/photos',
+            transformResponse: response => response.data.map(photo => ({
+                ...photo,
+                photo_url: `${url}/${photo.photo_url}`,
+                optimized_photo_url: `${url}/${photo.optimized_photo_url}`
+            }))
+        }),
+
+        getSearchResult: build.query({
+            query: (language, searchQuery) => ({
+                url: `${language}/search/${searchQuery}`,
+                method: 'POST',
+                body: searchQuery
+            }),
+            transformResponse: response => response.data
+        }),
     })
-  })
 })
 
 export const {
-  useGetPopularServicesQuery,
-  useGetAllCategoriesQuery,
-  useGetServicesQuery,
-  useGetPhotoGalleryQuery
+    useGetPopularServicesQuery,
+    useGetAllCategoriesQuery,
+    useGetServicesQuery,
+    useGetPhotoGalleryQuery,
+    useGetSearchResultQuery,
 } = apiSlice
