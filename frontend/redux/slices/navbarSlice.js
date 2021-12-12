78c0d8be74823027b543c56ada098759a35b1f6b
dmytroshylodev <dmytroshylodev@gmail.com>
2021-12-13 01:31:28 +0200

    deleted state for burger from navbarSlice

--- frontend/redux/slices/navbarSlice.js
+++ frontend/redux/slices/navbarSlice.js
@@ -4,19 +4,14 @@ const NavbarSlice = createSlice({
     name: 'navbar',
     initialState: {
         openDropdown: [],
-        isOpenBurgerMenu: false,
     },
     reducers: {
         openDropdownMenu: (state, action) => {
             state.openDropdown = action.payload;
-        },
-        openBurgerMenu: (state, action) => {
-            state.isOpenBurgerMenu = action.payload;
         }
     }
 })
 
-export const {openDropdownMenu, openBurgerMenu} = NavbarSlice.actions;
+export const {openDropdownMenu} = NavbarSlice.actions;
 export const openDropdown = state => state.navbar.openDropdown;
-export const isOpenBurgerMenu = state => state.navbar.isOpenBurgerMenu;
 export default NavbarSlice.reducer;
