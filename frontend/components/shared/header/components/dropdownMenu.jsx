c89f7c9b16a1286b37b1066d15a795abf9ed54a1
Shevchenko <shevchenkoandrey.dev@gmail.com>
2021-12-27 14:04:13 +0200

    Corrections from comments to PR

--- frontend/components/shared/header/components/dropdownMenu.jsx
+++ frontend/components/shared/header/components/dropdownMenu.jsx
@@ -1,26 +1,46 @@
 import styles from "../header.module.css";
 import DropdownItem from "./dropdownItem";
+import DropdownSubItem from "./dropdownSubItem";
 import {useDispatch, useSelector} from "react-redux";
-import {openDropdown, openDropdownMenu} from "@/redux/slices/navbarSlice";
+import {openSubDropdown, openDropdownSubMenu, openDropdown, openDropdownMenu} from "@/redux/slices/navbarSlice";
+import { useState } from "react";
 
-export default function DropdownMenu({category}) {
+export default function DropdownMenu({category, subcategory}) {
+    const openedDropdownSubMenu = useSelector(openSubDropdown);
     const openedDropdownMenu = useSelector(openDropdown);
     const dispatch = useDispatch();
 
     const handleOnClickCategory = (e) => {
         e.stopPropagation()
-        dispatch(openDropdownMenu(openedDropdownMenu.includes(e.target.id) ? [] : [e.target.id]));
+        if(subcategory) {
+            dispatch(openDropdownSubMenu(openedDropdownSubMenu.includes(e.target.id) ? [] : [e.target.id]));
+        } else {
+            dispatch(openDropdownMenu(openedDropdownMenu.includes(e.target.id) ? [] : [e.target.id]));
+        }
     }
 
+    const isOpenedDropdownSubMenu = openedDropdownSubMenu.includes(`${category.id}`);
     const isOpenedDropdownMenu = openedDropdownMenu.includes(`${category.id}`);
 
     return (
-        <div className={styles.dropdown_wrap}>
-            <a key={category.id} id={category.id} onClick={handleOnClickCategory}
-               className={`${styles.title} ${isOpenedDropdownMenu && styles.title_active}`}>
-                {category.title}
-            </a>
-            <DropdownItem category={category}/>
-        </div>
+        <>
+        {subcategory ?
+            <div className={`${styles.dropdown_wrap,  styles.sub_menu}`}>
+                <a key={category.id} id={category.id} onClick={handleOnClickCategory}
+                className={`${styles.title} ${isOpenedDropdownSubMenu && styles.title_active}`}>
+                    {category.title}
+                </a>
+                <DropdownSubItem category={category}/>
+            </div>
+            :
+            <div className={styles.dropdown_wrap}>
+                <a key={category.id} id={category.id} onClick={handleOnClickCategory}
+                className={`${styles.title} ${isOpenedDropdownMenu && styles.title_active}`}>
+                    {category.title}
+                </a>
+                <DropdownItem category={category}/>
+            </div>
+        }
+        </>
     )
 }
